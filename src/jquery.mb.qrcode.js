07b604226cf2ca6af022fb294620f52ef4a9239f
pupunzi <matbicoc@gmail.com>
2017-03-27 18:05:37 +0200

    First commit

---
+++ src/jquery.mb.qrcode.js
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ *
+ * jQuery.mb.components: jquery.mb.qrcode
+ * Creation date: 27/03/17
+ *
+ * Licences: MIT, GPL
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ ******************************************************************************/
+( function( $ ) {
+	$.mbQrcode = {
+		name: "jquery.mb.qrcode",
+		author: "Matteo Bicocchi (pupunzi)",
+		version: "1.0",
+		defaults: {},
+		init: function( options ) {}
+	}
+
+} )( jQuery );
